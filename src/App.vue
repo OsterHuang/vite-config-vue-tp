34e6a918c2b1a9057ac602021dc529aad3e4bb4e
TPI Oster Huang <oster.huang@tpisoftware.com>
2024-01-16 16:26:17 +0800

    Async Component chunked

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,24 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, defineAsyncComponent  } from 'vue'
 
 import AppHeader from './components/AppHeader.vue'
 
-import Home from './components/Home.vue'
-import HelloWorld from './components/HelloWorld.vue'
-import UserProfile from './components/UserProfile.vue'
-import CatGallery from './components/CatGallery.vue'
+// import Home from './components/Home.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import UserProfile from './components/UserProfile.vue'
+// import CatGallery from './components/CatGallery.vue'
 
 import { PageLink, AppLinks } from './model/AppPageModel'
+
+
 
 const chosePage = ref<PageLink>(AppLinks[0])
 const currentComponent = computed(() => {
   switch(chosePage.value.componentName) {
-    case "Home": return Home
-    case "HelloWorld": return HelloWorld
-    case "UserProfile": return UserProfile
-    case "CatGallery": return CatGallery
+    case "Home": return defineAsyncComponent(() => import('./components/Home.vue'))
+    case "HelloWorld": return defineAsyncComponent(() => import('./components/HelloWorld.vue'))
+    case "UserProfile": return defineAsyncComponent(() => import('./components/UserProfile.vue'))
+    case "CatGallery": return defineAsyncComponent(() => import('./components/CatGallery.vue'))
   }
   
 })
