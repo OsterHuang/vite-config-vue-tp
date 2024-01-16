7063863a6820c4d5cf3e4b4c22c5e9feb53a8306
TPI Oster Huang <oster.huang@tpisoftware.com>
2024-01-16 11:23:06 +0800

    alias path

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 import AppHeader from './components/AppHeader.vue'
 import UserProfile from './components/UserProfile.vue'
 import CateGallery from './components/CatGallery.vue'
@@ -13,7 +13,7 @@ import CateGallery from './components/CatGallery.vue'
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="@/assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
   <!-- <HelloWorld msg="Vite + Vue" /> -->
